3d48fdea928a55dbc7da12f31668f7a1ebb41aaf
ibrahemomari <ibrahem.omari96@gmail.com>
2021-09-13 18:56:07 +0300

    state

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,31 +1,69 @@
-import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
 import "./form.scss";
 
-function Form({ handleApiCall }) {
-  function handleSubmit(e) {
+function Form(props) {
+  const [textArea, setTextArea] = useState(false);
+  const [method, setMethod] = useState("get");
+  const [URL, setURL] = useState("");
+  const [request, setRequest] = useState("");
+
+  const handleSubmit =async (e) => {
     e.preventDefault();
-    const formData = {
-      method: "GET",
-      url: "https://pokeapi.co/api/v2/pokemon",
-    };
-    handleApiCall(formData);
-  }
+    try {
+      const formData = await axios({
+        method: method,
+        url: URL,
+      });
+      props.handleApiCall(formData, request);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const URLHandler = (e) => {
+    setURL(e.target.value);
+  };
+
+  const methodHandler = (e) => {
+    setMethod(e.target.id);
+    setTextArea(false);
+  };
+
+  const textAreaHandler = (e) => {
+    setTextArea(true);
+    setMethod(e.target.id);
+  };
+
+  const requestHandler = (e) => {
+    setRequest(e.target.value);
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
-          <input name="url" type="text" />
-          <button type="submit">GO!</button>
+          <input name="url" type="text" onChange={URLHandler} />
+          <button type="submit"  data-testid="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span id="get" onClick={methodHandler}>
+            GET
+          </span>
+          <span id="post" onClick={textAreaHandler}>
+            POST
+          </span>
+          <span id="put" onClick={textAreaHandler}>
+            PUT
+          </span>
+          <span id="delete" onClick={methodHandler}>
+            DELETE
+          </span>
         </label>
+        {textArea && (
+          <textarea rows="15" cols="35" onChange={requestHandler}></textarea>
+        )}
       </form>
     </>
   );
